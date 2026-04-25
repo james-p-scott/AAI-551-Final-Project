751f6986924913ae96caebdf4e42e225d7545c4e
james-p-scott <james-p-scott@users.noreply.github.com>
2026-04-25 16:51:45 -0700

    Create global_internet_access.py
    04/25/2026 - james.p.scott@boeing.com
    
    Initial version of global_internet_access.py that calls the class method in the file_io namespace that reads the ITU dataset from a .csv file and loads it into a Pandas dataframe.

---
+++ global_internet_access.py
@@ -0,0 +1,15 @@
+# import the file_io namespace
+import file_io
+
+def load_global_internet_access_data():
+    """
+    calls the class method from file_io namespace that loads the ITU global internet access
+    data from a .csv file into a Pandas dataframe
+    
+    :return: Pandas dataframe containing the ITU global internet access data
+    """
+    # load the ITU Individual Internet Usage data from the .csv file into a Pandas dataframe
+    df_loader = file_io.CSV_Load_DF("ITU-Data_Global-Internet-Access", "individuals-using-the-internet.csv")
+    # if the .csv file loaded successfully, display the head of the Pandas df
+    if df_loader.df is not None:
+        print(df_loader.df.head())
