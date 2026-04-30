b0c419bef6f181537dfa83d75f30a1db5870e6da
Jeffrey Busold <jlcbusold@gmail.com>
2026-04-30 12:06:46 -0400

    added ICT skill datasets; added digital_skills package with initial init; implemented ICTSkillsDataset class

---
+++ digital_skills/__init__.py
@@ -0,0 +1,11 @@
+"""
+digital_skills package
+
+Provides classes and functions to load, filter, and analyze ITU ICT skills data.
+"""
+
+from digital_skills.ict_skills_dataset import ICTSkillsDataset
+
+__all__ = [
+    "ICTSkillsDataset",
+]
